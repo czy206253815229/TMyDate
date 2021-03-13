c601f4ae9d37e0f5d70c66d02781af1517c3f03d
czy206253815229 <280978169@qq.com>
2021-03-13 14:52:03 +0800

    修复bug，简化代码

--- TMyDate/main.cpp
+++ TMyDate/main.cpp
@@ -61,27 +61,15 @@ void searchDate()
 }
 
 //遍历当前列表
-void Print(TNewDateList *tndl)
+template <class DataType>
+void Print(DataType*tndl)
 {
 	tndl->PrintList();
 }
-void Print(TNewDateLinkList* tndl)
-{
-	tndl->PrintList();
-}
+
 //判断给定日期是否设定为工作日
-void isWorkDay(TNewDateList *tndl)
-{
-	cout << "请输入要查询的日期：";
-	int y, m, d;
-	cin >> y >> m >> d;
-	TNewDate tndTemp = TNewDate(y, m, d);
-	if (tndl->Locate(tndTemp) != -1)
-		cout << "该日期是工作日" << endl;
-	else
-		cout << "该日期不是工作日或不在列表中" << endl;
-}
-void isWorkDay(TNewDateLinkList* tndl)
+template <class DataType>
+void isWorkDay(DataType*tndl)
 {
 	cout << "请输入要查询的日期：";
 	int y, m, d;
@@ -93,35 +81,23 @@ void isWorkDay(TNewDateLinkList* tndl)
 		cout << "该日期不是工作日或不在列表中" << endl;
 }
 //在列表中删除日期（标记为非工作日）
-void DeleteWorkDay(TNewDateList *tndl)
+template <class DataType>
+void DeleteWorkDay(DataType*tndl)
 {
 	cout << "请输入要改为非工作日的日期：";
 	int y, m, d;
 	cin >> y >> m >> d;
 	TNewDate tndTemp = TNewDate(y, m, d);
-	
-	tndTemp = tndl->Get(tndl->Locate(tndTemp));
-	
-	if (!tndTemp.isWorkDay())
-		cout << "该日期不在列表中，修改失败" << endl;
-	else
+
+	if (tndTemp.Interval(tndl->getStartTnd()) > 0 || tndTemp.Interval(tndl->getEndTnd()) < 0)
 	{
-		int location = tndl->Locate(tndTemp);
-		tndl->Get(location).setWorkDay(false);
-		tndl->Delete(location);
-		cout << "已设置为非工作日" << endl;
+		cout << "输入的日期超出范围" << endl;
+		return;
 	}
-}
-void DeleteWorkDay(TNewDateLinkList* tndl)
-{
-	cout << "请输入要改为非工作日的日期：";
-	int y, m, d;
-	cin >> y >> m >> d;
-	TNewDate tndTemp = TNewDate(y, m, d);
-
+	
 	tndTemp = tndl->Get(tndl->Locate(tndTemp));
-
-	if (!tndTemp.isWorkDay())
+	
+	if(tndl->Locate(tndTemp) == -1)
 		cout << "该日期不在列表中，修改失败" << endl;
 	else
 	{
@@ -132,16 +108,23 @@ void DeleteWorkDay(TNewDateLinkList* tndl)
 	}
 }
 //在列表中插入日期作为工作日
-void InsertWorkDay(TNewDateList *tndl)
+template <class DataType>
+void InsertWorkDay(DataType*tndl)
 {
 	cout << "请输入要改为工作日的日期：";
 	int y, m, d;
 	cin >> y >> m >> d;
 	TNewDate tndTemp = TNewDate(y, m, d);
 
-	if (tndTemp.isWorkDay())
+	if (tndTemp.Interval(tndl->getStartTnd()) > 0 || tndTemp.Interval(tndl->getEndTnd()) < 0)
 	{
-		cout << "该日期已在列表中或超出范围，修改失败" << endl;
+		cout << "输入的日期超出范围" << endl;
+		return;
+	}
+
+	if (tndl->Locate(tndTemp) != -1)
+	{
+		cout << "该日期已在列表中，修改失败" << endl;
 		return;
 	}
 	else
@@ -164,40 +147,9 @@ void InsertWorkDay(TNewDateList *tndl)
 		}
 	}
 }
-void InsertWorkDay(TNewDateLinkList* tndl)
-{
-	cout << "请输入要改为工作日的日期：";
-	int y, m, d;
-	cin >> y >> m >> d;
-	TNewDate tndTemp = TNewDate(y, m, d);
-
-	if (tndTemp.isWorkDay())
-	{
-		cout << "该日期已在列表中或超出范围，修改失败" << endl;
-		return;
-	}
-	else
-	{
-		tndTemp.setWorkDay(true);
-		int i;
-		for (i = 0; i < tndl->Length(); i++)
-		{
-			if (tndl->Get(i).Interval(tndTemp) < 0)
-			{
-				tndl->Insert(i, tndTemp);
-				cout << "该日期已成功插入列表" << endl;
-				break;
-			}
-		}
-		if (i == tndl->Length())
-		{
-			tndl->Insert(i + 1, tndTemp);
-			cout << "该日期已成功插入列表末位" << endl;
-		}
-	}
-}
 //一个时间段内一共有几个工作日
-void CountWorkDays(TNewDateList *tndl)
+template <class DataType>
+void CountWorkDays(DataType*tndl)
 {
 	cout << "请输入两个日期：" << endl;
 	cout << "请输入第一个日期:" << endl;
@@ -220,54 +172,13 @@ void CountWorkDays(TNewDateList *tndl)
 	else
 	{
 		int location1, location2;
-		while (!tndTemp1.isWorkDay())
+		while (tndl->Locate(tndTemp1) == -1)
 		{
-			tndTemp1.nextDay();
+			(TMyDate)tndTemp1 = tndTemp1.nextDay();
 		}
-		while (!tndTemp2.isWorkDay())
+		while (tndl->Locate(tndTemp2) == -1)
 		{
-			tndTemp2.frontDay();
-		}
-		location1 = tndl->Locate(tndTemp1);
-		location2 = tndl->Locate(tndTemp2);
-		if (location1 > location2)
-		{
-			cout << "两天之间没有工作日" << endl;
-		}
-		else
-			cout << "两天之间共有" << location2 - location1 + 1 << "天工作日" << endl;
-	}
-}
-void CountWorkDays(TNewDateLinkList* tndl)
-{
-	cout << "请输入两个日期：" << endl;
-	cout << "请输入第一个日期:" << endl;
-	int y, m, d;
-	cin >> y >> m >> d;
-	TNewDate tndTemp1 = TNewDate(y, m, d);
-	if (tndTemp1.Interval(tndl->getStartTnd()) > 0)
-	{
-		cout << "输入的起始日期超出范围！";
-		return;
-	}
-	cout << "请输入第二个日期:" << endl;
-	cin >> y >> m >> d;
-	TNewDate tndTemp2 = TNewDate(y, m, d);
-	if (tndTemp2.Interval(tndl->getEndTnd()) < 0)
-	{
-		cout << "输入的结束日期超出范围！";
-		return;
-	}
-	else
-	{
-		int location1, location2;
-		while (!tndTemp1.isWorkDay())
-		{
-			tndTemp1.nextDay();
-		}
-		while (!tndTemp2.isWorkDay())
-		{
-			tndTemp2.frontDay();
+			(TMyDate)tndTemp2 = tndTemp2.frontDay();
 		}
 		location1 = tndl->Locate(tndTemp1);
 		location2 = tndl->Locate(tndTemp2);
@@ -280,34 +191,8 @@ void CountWorkDays(TNewDateLinkList* tndl)
 	}
 }
 //主选单
-void menu(TNewDateList *tndl)
-{
-	cout << endl;
-
-	cout << "您需要执行什么操作？" << endl;
-	cout << "1.打印工作日列表" << endl;
-	cout << "2.判断某一天是否为工作日" << endl;
-	cout << "3.将一个日期设置为非工作日" << endl;
-	cout << "4.将一个日期设置为工作日" << endl;
-	cout << "5.计算两个日期间的工作日天数" << endl;
-	cout << endl;
-	cout << "----输入其它字符退出程序----" << endl;
-	cout << endl;
-
-	int choice;
-	cin >> choice;
-	cout << endl;
-	switch (choice)
-	{
-	case 1: {Print(tndl); Sleep(2000); menu(tndl); } break;
-	case 2: {isWorkDay(tndl); Sleep(2000); menu(tndl); } break;
-	case 3: {DeleteWorkDay(tndl); Sleep(2000); menu(tndl); } break;
-	case 4: {InsertWorkDay(tndl); Sleep(2000); menu(tndl); } break;
-	case 5: {CountWorkDays(tndl); Sleep(2000); menu(tndl); } break;
-	default: exit(0); break;
-	}
-}
-void menu(TNewDateLinkList* tndl)
+template <class DataType>
+void menu(DataType*tndl)
 {
 	cout << endl;
 
